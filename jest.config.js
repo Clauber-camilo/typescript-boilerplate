085eba5c013b2629a89438af6496408388767ab4
Clauber Camilo <clauber_camilo@live.com>
2025-09-14 21:08:22 -0300

    update ci

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,36 @@
-const { pathsToModuleNameMapper } = require("ts-jest");
+const { pathsToModuleNameMapper } = require('ts-jest')
 
-const { compilerOptions } = require("./tsconfig.default.json");
+const { compilerOptions } = require('./tsconfig.default.json')
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-	preset: "ts-jest",
-	testEnvironment: "jest-environment-node",
-	verbose: true,
-	transform: {
-		// '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
-		// '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
-		"^.+\\.tsx?$": [
-			"ts-jest",
-			{
-				// ts-jest configuration goes here
-				tsconfig: "./tsconfig.spec.json",
-				isolatedModules: true,
-			},
-		],
-	},
-	setupFiles: ["./src/test-setup.ts"],
-	coveragePathIgnorePatterns: ["<rootDir>/node_modules/", ".mock.js$"],
-	modulePaths: [compilerOptions.baseUrl],
-	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-		prefix: "<rootDir>",
-	}),
-};
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-node',
+  verbose: true,
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        // ts-jest configuration goes here
+        tsconfig: './tsconfig.spec.json',
+        isolatedModules: true
+      }
+    ]
+  },
+  setupFiles: ['./src/test-setup.ts'],
+  coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '.mock.js$'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10
+    }
+  },
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>'
+  })
+}
